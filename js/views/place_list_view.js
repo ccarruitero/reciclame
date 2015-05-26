ad4105c159f3668655f021f4b64a152c4ff6379d
ccarruitero <cesar@mozilla.pe>
2015-05-26 01:06:22 -0500

    add markers in place list view

--- js/views/place_list_view.js
+++ js/views/place_list_view.js
@@ -8,13 +8,33 @@ define([
   var PlaceListView = React.createClass({
     mixins: [ReactBackboneMixin],
 
-    addPlace: function(model) {
-      return React.createElement("li", {key: model.id}, model.name);
+    addMarker: function(place){
+      var lat = place.lat;
+      var lng = place.lng;
+      var that = this;
+
+      L.mapbox.featureLayer({
+          type: 'Feature',
+          geometry: {
+              type: 'Point',
+              coordinates: [lng, lat]
+          },
+          properties: {
+              'marker-color': '#000',
+              'marker-symbol': 'waste-basket',
+              'marker-size': 'medium',
+              title: place.name,
+              address: place.address
+          }
+      }).addTo(map).on('click', function(e) {
+          var path = '/places/' + place.id;
+          Backbone.history.navigate(path, true);
+      });
     },
 
     render: function() {
       var that = this;
-      return React.createElement("div", null, this.state.collection.map(this.addPlace));
+      return React.createElement("div", {}, this.state.collection.map(this.addMarker));
     }
   });
 
